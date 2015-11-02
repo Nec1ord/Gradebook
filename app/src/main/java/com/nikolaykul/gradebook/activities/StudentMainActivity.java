df68b18a61077d2910b7fea851ca78739b333b5e
Kulachenko Nikolai <gN1kolaykul@gmail.com>
2015-11-02 17:59:12 +0300

    Inflate toolbar and add TODO.

--- app/src/main/java/com/nikolaykul/gradebook/activities/StudentMainActivity.java
+++ app/src/main/java/com/nikolaykul/gradebook/activities/StudentMainActivity.java
@@ -2,28 +2,58 @@ package com.nikolaykul.gradebook.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.nikolaykul.gradebook.R;
 import com.nikolaykul.gradebook.data.local.Database;
 import com.nikolaykul.gradebook.data.models.Student;
-import com.nikolaykul.gradebook.di.HasComponent;
-import com.nikolaykul.gradebook.di.component.ActivityComponent;
-
-import java.util.List;
 
 import javax.inject.Inject;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class StudentMainActivity extends BaseActivity {
+    @Bind(R.id.toolbar) Toolbar mToolbar;
+    @Inject Database mDatabase;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_student: addStudent(); break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        ButterKnife.unbind(this);
+        super.onDestroy();
     }
 
     @Override
     protected void setActivityComponent() {
         getActivityComponent().inject(this);
+    }
+
+    private void addStudent() {
+        // TODO
     }
 
 }
