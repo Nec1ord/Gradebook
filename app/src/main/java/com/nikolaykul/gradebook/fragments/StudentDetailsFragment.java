1d225aae336c7129c258cf9db087e2ab94e5ad91
Kulachenko Nikolai <gN1kolaykul@gmail.com>
2015-11-06 15:10:35 +0300

    Change populating table.

--- app/src/main/java/com/nikolaykul/gradebook/fragments/StudentDetailsFragment.java
+++ app/src/main/java/com/nikolaykul/gradebook/fragments/StudentDetailsFragment.java
@@ -4,19 +4,26 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
-import android.widget.TableRow.LayoutParams;
+import android.widget.TextView;
 
 import com.nikolaykul.gradebook.R;
 import com.nikolaykul.gradebook.data.local.Database;
 import com.nikolaykul.gradebook.data.models.Student;
 import com.nikolaykul.gradebook.data.models.StudentInfo;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import javax.inject.Inject;
 
@@ -24,9 +31,46 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class StudentDetailsFragment extends BaseFragment {
+    private static final String BUNDLE_INFO_TABLE = "infoTable";
     @Bind(R.id.table) TableLayout mTable;
+    @Bind(R.id.students_column) LinearLayout mColumnStudents;
     @Inject Context mContext;
     @Inject Database mDatabase;
+    private short mStudentInfoTable;
+    private List<Student> mStudents;
+    private float mStudentsTextSize;
+    private int mRowViewHeight;
+    private int mRowViewWidth;
+
+    public static StudentDetailsFragment getInstance(short infoTable) {
+        StudentDetailsFragment fragment = new StudentDetailsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putShort(BUNDLE_INFO_TABLE, infoTable);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        getActivityComponent().inject(this);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+        if (savedInstanceState != null) {
+            mStudentInfoTable = savedInstanceState.getByte(BUNDLE_INFO_TABLE);
+        } else {
+            mStudentInfoTable = Database.STUDENT_ATTENDANCE;
+        }
+        mStudents = mDatabase.getStudents();
+        mRowViewWidth = (int) getResources().getDimension(R.dimen.table_row_view_width);
+        mRowViewHeight = (int) getResources().getDimension(R.dimen.table_row_view_height);
+        mStudentsTextSize = getResources().getDimension(R.dimen.table_students_text_size);
+    }
 
     @Nullable
     @Override
@@ -34,14 +78,19 @@ public class StudentDetailsFragment extends BaseFragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_student_details, container, false);
         ButterKnife.bind(this, view);
+        refreshContainers();
         return view;
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        getActivityComponent().inject(this);
-        populateTable();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_student_info:
+                addInfo();
+                refreshContainers();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -50,35 +99,103 @@ public class StudentDetailsFragment extends BaseFragment {
         super.onDestroyView();
     }
 
-    private void populateTable() {
-        List<Student> studentList = mDatabase.getStudents();
-        for (Student student : studentList) {
-            mTable.addView(createRow(student.id));
+    // TODO
+    private void addInfo() {
+        mDatabase.insertStudentInfo(new Date(), mStudentInfoTable);
+    }
+
+    private void refreshContainers() {
+        // clear
+        mColumnStudents.removeAllViews();
+        mTable.removeAllViews();
+
+        // populate
+        populateHeader();
+        for (Student student : mStudents) {
+            // populate LinearLayout (students)
+            mColumnStudents.addView(createViewStudentName(student.fullName));
+
+            // populate TableLayout (content)
+            mTable.addView(createRowContent(student.id));
             mTable.addView(createDivider(true));
         }
     }
 
-    private TableRow createRow(long studentId) {
+    private void populateHeader() {
+        mColumnStudents.addView(createViewEmpty());
+        mTable.addView(createRowHeader());
+    }
+
+    private TableRow createRowHeader() {
         TableRow row = new TableRow(mContext);
 
-        List<StudentInfo> infoList = mDatabase.getStudentInfo(studentId, Database.STUDENT_ATTENDANCE);
+        long someStudentId = mDatabase.getStudents().get(0).id;
+        List<StudentInfo> infoList = mDatabase.getStudentInfo(someStudentId, mStudentInfoTable);
         for (StudentInfo info : infoList) {
-            row.addView(createRowView(info.wasGood));
+            row.addView(createViewHeader(info.date));
             row.addView(createDivider(false));
         }
-
         return row;
     }
 
-    private View createRowView(boolean wasGood) {
-        int width = (int) getResources().getDimension(R.dimen.table_row_view_width);
-        int height = (int) getResources().getDimension(R.dimen.table_row_view_height);
+    private TableRow createRowContent(long studentId) {
+        TableRow row = new TableRow(mContext);
+        List<StudentInfo> infoList = mDatabase.getStudentInfo(studentId, mStudentInfoTable);
+        for (StudentInfo info : infoList) {
+            row.addView(createViewContent(info));
+            row.addView(createDivider(false));
+        }
+        return row;
+    }
 
+    private TextView createViewStudentName(String studentName) {
+        TextView tv = new TextView(mContext);
+        tv.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                mRowViewHeight));
+        tv.setGravity(Gravity.CENTER);
+        tv.setSingleLine();
+        tv.setTextSize(mStudentsTextSize);
+        tv.setText(studentName);
+        return tv;
+    }
+
+    private TextView createViewHeader(Date date) {
+        DateFormat df = new SimpleDateFormat("dd/MM", Locale.getDefault());
+
+        TextView tv = new TextView(mContext);
+        tv.setLayoutParams(new TableRow.LayoutParams(mRowViewWidth, mRowViewHeight));
+        tv.setGravity(Gravity.CENTER);
+        tv.setSingleLine();
+        tv.setTextSize(mStudentsTextSize);
+        tv.setText(df.format(date));
+        return tv;
+    }
+
+    private View createViewContent(StudentInfo info) {
         View view = new View(mContext);
-        view.setLayoutParams(new LayoutParams(width, height));
-        view.setBackgroundColor(ContextCompat.getColor(mContext, wasGood
+        view.setLayoutParams(new TableRow.LayoutParams(mRowViewWidth, mRowViewHeight));
+        view.setBackgroundColor(ContextCompat.getColor(mContext, info.wasGood
                 ? R.color.green
                 : R.color.red));
+        view.setTag(info);
+        view.setOnClickListener(iView -> {
+            StudentInfo currentInfo = (StudentInfo) view.getTag();
+            currentInfo.wasGood = !currentInfo.wasGood;
+            view.setBackgroundColor(ContextCompat.getColor(mContext, currentInfo.wasGood
+                    ? R.color.green
+                    : R.color.red));
+            mDatabase.updateStudentInfo(currentInfo, mStudentInfoTable);
+            view.setTag(currentInfo);
+        });
+        return view;
+    }
+
+    private View createViewEmpty() {
+        View view = new View(mContext);
+        view.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                mRowViewHeight));
         return view;
     }
 
@@ -87,11 +204,11 @@ public class StudentDetailsFragment extends BaseFragment {
      * @return view that represents a divider.
      */
     private View createDivider(boolean isHorizontalDivider) {
-        int width  = isHorizontalDivider ? LayoutParams.MATCH_PARENT : 1;
-        int height = isHorizontalDivider ? 1 : LayoutParams.MATCH_PARENT;
+        int width  = isHorizontalDivider ? TableRow.LayoutParams.MATCH_PARENT : 1;
+        int height = isHorizontalDivider ? 1 : TableRow.LayoutParams.MATCH_PARENT;
 
         View view = new View(mContext);
-        view.setLayoutParams(new LayoutParams(width, height));
+        view.setLayoutParams(new TableRow.LayoutParams(width, height));
         view.setBackgroundColor(ContextCompat.getColor(mContext, R.color.gray));
         return view;
     }
