f56a3e910091bfc8dc3fa9dd940f69b4e87eff47
Kulachenko Nikolai <gN1kolaykul@gmail.com>
2015-11-13 14:54:25 +0300

    Create scrolling behavior for FAB.

---
+++ app/src/main/java/com/nikolaykul/gradebook/utils/ScrollAwareFABBehavior.java
@@ -0,0 +1,55 @@
+package com.nikolaykul.gradebook.utils;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ *  Custom behavior for FloatingActionButton.
+ *  Hides FAB on scroll down.
+ *  Shows FAB on scroll up.
+ */
+public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
+
+    /**
+     * Create the constructor to set this behavior via XML
+     */
+    public ScrollAwareFABBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
+                                       FloatingActionButton child,
+                                       View directTargetChild,
+                                       View target,
+                                       int nestedScrollAxes) {
+        boolean bSuper = super.onStartNestedScroll(
+                coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+        // Ensure we react to vertical scrolling
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL || bSuper;
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout,
+                               FloatingActionButton child,
+                               View target,
+                               int dxConsumed, int dyConsumed,
+                               int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(
+                coordinatorLayout, child, target,
+                dxConsumed, dyConsumed,
+                dxUnconsumed, dyUnconsumed);
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            // User scrolled down and the FAB is currently visible -> hide the FAB
+            child.hide();
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            // User scrolled up and the FAB is currently not visible -> show the FAB
+            child.show();
+        }
+    }
+
+}
