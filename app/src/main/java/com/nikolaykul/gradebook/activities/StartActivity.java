0b1d0d1b18364ba7af14a7026bce7f317bfb3aba
Kulachenko Nikolai <gN1kolaykul@gmail.com>
2015-11-15 17:09:32 +0300

    Add tabs.

--- app/src/main/java/com/nikolaykul/gradebook/activities/StartActivity.java
+++ app/src/main/java/com/nikolaykul/gradebook/activities/StartActivity.java
@@ -2,27 +2,47 @@ package com.nikolaykul.gradebook.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 
 import com.nikolaykul.gradebook.R;
+import com.nikolaykul.gradebook.adapters.SimplePagerAdapter;
 import com.nikolaykul.gradebook.data.events.FloatingActionButtonEvent;
+import com.nikolaykul.gradebook.data.local.Database;
 import com.nikolaykul.gradebook.data.models.StudentGroup;
+import com.nikolaykul.gradebook.fragments.StudentGroupListFragment;
+import com.nikolaykul.gradebook.fragments.StudentInfoFragment;
+import com.nikolaykul.gradebook.fragments.StudentListFragment;
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
 
 import javax.inject.Inject;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import timber.log.Timber;
 
 public class StartActivity extends BaseActivity {
+    private static final String BUNDLE_GROUP_ID = "group_id";
+    private static final String BUNDLE_GROUP_NAME = "group_name";
+    @Bind(R.id.collapsingToolbarLayout) CollapsingToolbarLayout mCollapsingLayout;
+    @Bind(R.id.toolbar) Toolbar mToolbar;
+    @Bind(R.id.viewPager) ViewPager mViewPager;
+    @Bind(R.id.tabs) TabLayout mTabs;
     @Inject Bus mBus;
+    private long mSelectedGroupId;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         ButterKnife.bind(this);
+        init(savedInstanceState);
+        setSupportActionBar(mToolbar);
+        setViewPager(mViewPager);
+        mTabs.setupWithViewPager(mViewPager);
     }
 
     @Override
@@ -43,6 +63,16 @@ public class StartActivity extends BaseActivity {
     }
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        CharSequence title = mCollapsingLayout.getTitle();
+        if (null != title) {
+            outState.putString(BUNDLE_GROUP_NAME, title.toString());
+        }
+        outState.putLong(BUNDLE_GROUP_ID, mSelectedGroupId);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
     protected void onDestroy() {
         ButterKnife.unbind(this);
         super.onDestroy();
@@ -53,7 +83,41 @@ public class StartActivity extends BaseActivity {
     }
 
     @Subscribe public void OnGroupClicked(StudentGroup group) {
-        Timber.i("group: id = %d, name = %s", group.id, group.name);
+        mSelectedGroupId = group.id;
+        mCollapsingLayout.setTitle(group.name);
+    }
+
+    private void init(Bundle savedState) {
+        if (null != savedState) {
+            String title = savedState.getString(BUNDLE_GROUP_NAME, "");
+            if (!title.isEmpty()) {
+                mCollapsingLayout.setTitle(title);
+            }
+            mSelectedGroupId = savedState.getLong(BUNDLE_GROUP_ID, 0);
+        } else {
+            mCollapsingLayout.setTitle("");
+            mSelectedGroupId = 0;
+        }
+    }
+
+    private void setViewPager(ViewPager viewPager) {
+        SimplePagerAdapter adapter = new SimplePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(
+                new StudentGroupListFragment(),
+                "Groups");
+        adapter.addFragment(
+                StudentListFragment.getInstance(mSelectedGroupId),
+                "Students");
+        adapter.addFragment(
+                StudentInfoFragment.getInstance(Database.STUDENT_ATTENDANCE, mSelectedGroupId),
+                "Attendance");
+        adapter.addFragment(
+                StudentInfoFragment.getInstance(Database.STUDENT_PRIVATE_TASK, mSelectedGroupId),
+                "Private tasks");
+        adapter.addFragment(
+                StudentInfoFragment.getInstance(Database.STUDENT_TEST, mSelectedGroupId),
+                "Tests");
+        viewPager.setAdapter(adapter);
     }
 
 }
---
+++ app/src/main/java/com/nikolaykul/gradebook/adapters/SimplePagerAdapter.java
@@ -0,0 +1,37 @@
+package com.nikolaykul.gradebook.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class SimplePagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private ArrayList<String> mTitles = new ArrayList<>();
+
+    public SimplePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitles.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragments.add(fragment);
+        mTitles.add(title);
+    }
+
+}
