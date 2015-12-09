e6a695b4b092673a0cb17a97c087aa7848de5482
Nikolay <gN1kolaykul@gmail.com>
2015-12-09 17:57:17 +0300

    Morph TextView into EditText and change callbacks.

--- app/src/main/java/com/nikolaykul/gradebook/adapter/ModelAdapter.java
+++ app/src/main/java/com/nikolaykul/gradebook/adapter/ModelAdapter.java
@@ -2,12 +2,16 @@ package com.nikolaykul.gradebook.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import com.nikolaykul.gradebook.R;
+import com.nikolaykul.gradebook.data.local.Database;
 import com.nikolaykul.gradebook.data.local.MPreferences;
 import com.nikolaykul.gradebook.data.local.MPreferences.ModelPreferencesFactory;
 import com.nikolaykul.gradebook.data.model.Model;
@@ -20,14 +24,18 @@ public class ModelAdapter extends RecyclerView.Adapter<ModelAdapter.ViewHolder> 
     private int mLastSelectedItemPosition;
     private List<Model> mModels;
     private Bus mBus;
+    private Database mDatabase;
+    private Context mContext;
 
-    public ModelAdapter(Context context, List<Model> models, Bus bus) {
+    public ModelAdapter(Context context, List<Model> models, Bus bus, Database database) {
         if (!models.isEmpty()) {
             mModelPreferencesFactory = new MPreferences(context).with(models.get(0));
             mLastSelectedItemPosition = mModelPreferencesFactory.getLastSelectedPosition();
         }
         mModels = models;
         mBus = bus;
+        mDatabase = database;
+        mContext = context;
     }
 
     @Override
@@ -40,24 +48,12 @@ public class ModelAdapter extends RecyclerView.Adapter<ModelAdapter.ViewHolder> 
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.tvText.setText(mModels.get(position).getTitle());
-        holder.tvText.setSelected(mLastSelectedItemPosition == position);
-        holder.tvText.setOnClickListener(view -> {
-            //  Prevent the selection of the last item in the adapter
-            // right after some item was just deleted from the list.
-            // Other way it may throw IndexOutOfBoundsException.
-            if (position >= getItemCount()) return;
-
-            if (mLastSelectedItemPosition != position){
-                notifyItemChanged(mLastSelectedItemPosition);               // remove prev selection
-                mLastSelectedItemPosition = position;
-                holder.tvText.setSelected(true);
-                if (null != mModelPreferencesFactory) {
-                    mModelPreferencesFactory.putLastSelectedPosition(position); // store selection
-                }
-            }
-            mModels.get(position).notifyClicked(mBus);
-        });
+        holder.etTitle.setText(mModels.get(position).getTitle());
+        holder.etTitle.setSelected(mLastSelectedItemPosition == position);
+        // set callbacks
+        final GestureDetector detector = new GestureDetector(mContext,
+                new GestureListener(holder.etTitle, position));
+        holder.etTitle.setOnTouchListener((view, event) -> detector.onTouchEvent(event));
     }
 
     @Override
@@ -66,11 +62,72 @@ public class ModelAdapter extends RecyclerView.Adapter<ModelAdapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public final TextView tvText;
+        public final EditText etTitle;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            tvText = (TextView) itemView;
+            etTitle = (EditText) itemView;
+            etTitle.setInputType(InputType.TYPE_NULL);
+        }
+    }
+
+    private class GestureListener extends GestureDetector.SimpleOnGestureListener {
+        private final EditText mView;
+        private final int mPosition;
+        private final Model mItem;
+
+        public GestureListener(EditText view, int position) {
+            mView = view;
+            mPosition = position;
+            mItem = mModels.get(position);
+        }
+
+        /**
+         *  Remove previous selection {@see #notifyItemChanged},
+         * store current {@link #mPosition} in {@link #mLastSelectedItemPosition},
+         * set {@link #mView} selected and notify observers {@see #mItem.notifyClicked}.
+         * */
+        @Override public boolean onSingleTapConfirmed(MotionEvent e) {
+            //  Prevent the selection of the last item in the adapter
+            // right after some item was just deleted from the list.
+            // Other way it may throw IndexOutOfBoundsException.
+            if (mPosition >= getItemCount()) return true;
+
+            if (mLastSelectedItemPosition != mPosition){
+                notifyItemChanged(mLastSelectedItemPosition);   // remove prev selection
+                mLastSelectedItemPosition = mPosition;
+                mView.setSelected(true);
+                if (null != mModelPreferencesFactory) {
+                    mModelPreferencesFactory.putLastSelectedPosition(mPosition); // store selection
+                }
+            }
+            mItem.notifyClicked(mBus);
+            return true;
+        }
+
+        /**
+         *  Set {@link #mView} editable. When focus has changed set {@link #mView} NOT editable and
+         *  remove the focus listener. If {@see #mView.getText} is not empty ->
+         *  set new title {@see #mItem.setTitle}, update the database {@see #mItem.updateInDatabase}
+         *  and notify observers {@see #mItem.notifyUpdated}.
+         * */
+        @Override public boolean onDoubleTap(MotionEvent e) {
+            mView.setInputType(InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+            mView.selectAll();
+            mView.setOnFocusChangeListener((v, hasFocus) -> {
+                if (!hasFocus) {
+                    mView.setInputType(InputType.TYPE_NULL);
+                    mView.setOnFocusChangeListener(null);
+                    String newTitle = mView.getText().toString();
+                    if (!newTitle.isEmpty()) {
+                        mItem.setTitle(newTitle);
+                        mItem.updateInDatabase(mDatabase);
+                        mItem.notifyUpdated(mBus);
+                        notifyItemChanged(mPosition);
+                    }
+                }
+            });
+            return true;
         }
     }
 
