d4304f42430cf255156e5231987784370dcce191
Nikolay <gN1kolaykul@gmail.com>
2015-12-13 19:44:24 +0300

    Remove the Information field.

--- app/src/main/java/com/nikolaykul/gradebook/event/InformationAddedEvent.java
+++ app/src/main/java/com/nikolaykul/gradebook/event/InformationAddedEvent.java
@@ -1,15 +1,11 @@
 package com.nikolaykul.gradebook.event;
 
 import com.nikolaykul.gradebook.data.local.Database;
-import com.nikolaykul.gradebook.data.model.Information;
 
 public class InformationAddedEvent {
     @Database.InformationTable public final int TABLE;
-    public final Information info;
 
-    public InformationAddedEvent(@Database.InformationTable final int table,
-                                 final Information info) {
+    public InformationAddedEvent(@Database.InformationTable final int table) {
         this.TABLE = table;
-        this.info = info;
     }
 }
